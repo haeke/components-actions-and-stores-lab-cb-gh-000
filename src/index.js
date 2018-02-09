fc886acf95f3a8db6422e1a91d9e7994e29cd0d4
edwin rivera <waehake@gmail.com>
2018-02-09 14:26:02 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from './store';
+import bandReducer from './reducers/band_reducer';
 
-const store = createStore(reducer);
+const store = createStore(bandReducer);
 
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'));
