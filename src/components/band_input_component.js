7ba31a12de1240f2fb1f820c7924ed6874e50a2a
edwin rivera <waehake@gmail.com>
2018-02-09 14:36:34 +0000

    automatically backed up by learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  onSubmit() {
+    console.log('submitted something');
+  }
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label>Band label: </label>
           <input />
           <button type="submit">Submit</button>
