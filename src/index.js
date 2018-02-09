bb92f23b2019087ecce71cf46f5dc695b6c9b000
edwin rivera <waehake@gmail.com>
2018-02-09 14:21:45 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from './store';
 
+const store = createStore(reducer);
+
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'));
 }
