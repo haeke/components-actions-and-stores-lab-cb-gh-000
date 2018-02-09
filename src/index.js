d2baec6d6999ac6607aa99070111ffd101514786
edwin rivera <waehake@gmail.com>
2018-02-09 14:14:14 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listners.forEach(listener => listener());
-  };
-
-  const subscribe = (listener) => {
-    listener.push(listener);
-  };
-
-  dispatch({});
-  return {
-    getState: getState,
-    dispatch: dispatch,
-    subscribe: subscribe,
-  };
-};
-
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'));
 }
