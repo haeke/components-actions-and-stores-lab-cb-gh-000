287f935dc857138f86bf28b534d64e76cb41589a
edwin rivera <waehake@gmail.com>
2018-02-09 15:12:13 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import BandInput from './components/band_input_component';
 
 const store = createStore(bandReducer);
 
+//A component will need to re-render when state is changed to display new information. We need to wrap the ReactDOM.render() in a renderApp function that we pushed to our listners array called each time dispatch is called. We will need this functionality so that whenever a new band is added to state our whole application re-renders so that our band_index_component shows the updated bands.
+
 const renderApp = () => {
   ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'));
 };
